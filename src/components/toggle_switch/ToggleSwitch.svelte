2c7ba75d97097b21d95516799ddcc9e2aa5a3886
Parables Boltnoel <parables95@gmail.com>
2020-07-09 20:40:29 +0000

    Improved ToggleSwitch

--- src/components/toggle_switch/ToggleSwitch.svelte
+++ src/components/toggle_switch/ToggleSwitch.svelte
@@ -1,53 +1,27 @@
 <script>
-  import { createEventDispatcher } from 'svelte'
-  let dispatch = createEventDispatcher()
-  export let checked = false;
-  export let variant = 'outer'; //inner''
-  export let withText = false
+  import { createEventDispatcher } from 'svelte';
+  let dispatch = createEventDispatcher();
+  export let value = false;
+  export let variant = 'inner'; //'outer'
+  export let withText = true;
 </script>
 
-{#if variant === 'inner'}
-  <div
-    class="relative inline-flex {withText? 'w-auto': 'w-12'} h-7 border-transparent rounded-full {checked ? 'bg-success' : 'bg-comet'}"
-    on:click  on:click="{() => {
-       checked = !checked 
-       dispatch('toggle', checked)
-    }}">
-
-     {#if withText}
-     <div class="flex items-center justify-between px-2 py-0 m-0 ">
-         <slot>
-           <span class="p-0 mr-2 text-xs leading-normal lowercase align-text-top select-none text-cararra">on</span>
-           <span class="p-0 m-0 text-xs leading-normal lowercase align-text-top select-none text-cararra">off</span>
-        </slot>
-     </div>
-     {/if}
-    <div
-      class="absolute w-7 h-7 border rounded-full {checked ? 'border-success' : 'border-comet'}
-      bg-cararra transform transition-all duration-300 ease-linear {checked ?'translate-x-full' : 'translate-x-0'}"
-    ></div>
-  </div>
-  {:else}
-  <div class="relative inline-block " on:click
-   on:click="{() => {
-    checked = !checked 
-    dispatch('toggle', checked)
-  }}" >
-    <div
-      class="absolute top-2 {withText? 'w-auto': 'w-12'} h-5 border-transparent rounded-full {checked ? 'bg-success' : 'bg-comet'}"
-    >
-    {#if withText}
-    <div class="box-content flex items-center justify-between px-2 py-0 m-0">
-       <slot>
-           <span class="p-0 mr-2 text-xs leading-normal lowercase align-text-top select-none text-cararra">on</span>
-           <span class="p-0 m-0 text-xs leading-normal lowercase align-text-top select-none text-cararra">off</span>
-        </slot>
-      </div>
-    {/if}
-  </div>
-    <div
-      class="absolute top-1 w-7 h-7 border rounded-full {checked ? 'border-success' : 'border-comet'}
-      bg-cararra transform transition-all duration-300 ease-linear {checked ? 'translate-x-full' : 'translate-x-0'}"
-    ></div>
-  </div>
-{/if}
+<div
+  class=" inline-flex items-center rounded-full w-12
+  {variant === 'inner' ? 'h-5' : 'h-3'}
+  {value ? 'bg-success' : 'bg-comet'}"
+  on:click
+  on:click="{() => {
+    value = !value;
+    dispatch('toggle', value);
+  }}"
+>
+  {#if withText}
+  <span class="px-1 text-xs leading-normal lowercase align-text-top select-none text-cararra">on</span>
+  <span class="px-1 text-xs leading-normal lowercase align-text-top select-none text-cararra">off</span>
+  {/if}
+  <span
+    class="absolute w-5 h-5 border rounded-full {value ? 'border-success' : 'border-comet'}
+    bg-cararra transform transition-all duration-300 ease-linear {value ? 'translate-x-7' : 'translate-x-0'}"
+  ></span>
+</div>
